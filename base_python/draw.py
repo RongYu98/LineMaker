3a619866e648a01ea23369ab6483dd26d502f906
RongYu <stuy.rong.yu@gmail.com>
2016-02-11 11:47:22 -0500

    added dw source code

---
+++ base_python/draw.py
@@ -0,0 +1,16 @@
+from display import *
+
+def draw_line( screen, x0, y0, x1, y1, color ):
+
+    
+    #octent 1
+    if (x0 > x1 && y0 > y1):#octent 5
+        pass;
+    #(x0,y0) --> (x1, y1)
+
+    x = x0
+    y = y0
+    A = y1
+    d = 2A+2B
+    
+    pass
